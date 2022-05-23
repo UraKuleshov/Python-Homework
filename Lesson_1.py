e9e8612af107eb3a8172b97a3040b2674c8c65df
Юрий Кулешов <kulesovurij817@gmail.com>
2022-05-23 20:43:00 +0300

    task3

--- Lesson_1.py
+++ Lesson_1.py
@@ -22,6 +22,7 @@ elif duration > 86400:
     seconds = duration - minutes * 60
     print(int(days), "дн", int(hour), "час", int(minutes), "мин", int(seconds), "сек")
 
+
 # task 2
 cub_numbers = [i**3 for i in range(1, 1001) if i % 2 != 0]
 print(cub_numbers)
@@ -31,13 +32,11 @@ count = 0
 b2 = [count + i for i in cub_numbers if sum(map(int, str(i))) % 7 == 0]
 print(sum(b2))
 
-
 # task2_b
 count = 0
 summa = [i + 17 for i in cub_numbers]
 sum_of_numbers = [count + i for i in summa if sum(map(int, str(i))) % 7 == 0]
 print(sum(sum_of_numbers))
-
 
 # task2_c
 score = 0
@@ -49,3 +48,18 @@ for i in cub_numbers:
         cub_numbers[score] = i
     score = score + 1
 print(sum(cub_numbers))
+
+
+# task 3
+single_digits = {0: "процентов", 1: "процент", 2: "процента", 3: "процента", 4: "процента", 5: "процентов",
+                 6: "процентов", 7: "процентов", 8: "процентов", 9: "процентов", 10: "процентов"}
+
+for number in range(1, 101):
+    if number <= 10:
+        print(number, single_digits[number])
+    elif number < 20 and number > 10:
+        print(number, "процентов")
+    else:
+        number = str(number)
+        slice_number = number[1]
+        print(number, single_digits[int(slice_number)])

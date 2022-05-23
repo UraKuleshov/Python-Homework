82d15779a89051892f7be6898727eb4db542ab25
Юрий Кулешов <kulesovurij817@gmail.com>
2022-05-23 20:40:21 +0300

    task2

--- Lesson_1.py
+++ Lesson_1.py
@@ -1,3 +1,4 @@
+# task 1
 duration = float(input("Введите количество секунд: "))
 
 if duration <= 60:
@@ -20,3 +21,31 @@ elif duration > 86400:
     minutes = duration // 60
     seconds = duration - minutes * 60
     print(int(days), "дн", int(hour), "час", int(minutes), "мин", int(seconds), "сек")
+
+# task 2
+cub_numbers = [i**3 for i in range(1, 1001) if i % 2 != 0]
+print(cub_numbers)
+
+# tack2_a
+count = 0
+b2 = [count + i for i in cub_numbers if sum(map(int, str(i))) % 7 == 0]
+print(sum(b2))
+
+
+# task2_b
+count = 0
+summa = [i + 17 for i in cub_numbers]
+sum_of_numbers = [count + i for i in summa if sum(map(int, str(i))) % 7 == 0]
+print(sum(sum_of_numbers))
+
+
+# task2_c
+score = 0
+for i in cub_numbers:
+    i = i + 17
+    if sum(map(int, str(i))) % 7 != 0:
+        cub_numbers[score] = 0
+    elif sum(map(int, str(i))) % 7 == 0:
+        cub_numbers[score] = i
+    score = score + 1
+print(sum(cub_numbers))

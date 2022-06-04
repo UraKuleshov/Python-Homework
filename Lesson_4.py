0af0cec4cc8d1c94585a40f9d11d89fa7059f0ae
Юрий Кулешов <kulesovurij817@gmail.com>
2022-06-04 17:00:54 +0300

    First commit

---
+++ Lesson_4.py
@@ -0,0 +1,6 @@
+import currency_rates
+import sys
+
+command = sys.argv[1]
+
+currency_rates.currency_rates(command)
---
+++ currency_rates.py
@@ -0,0 +1,19 @@
+import requests
+import json
+
+
+def currency_rates(currency):
+    response = requests.get('https://www.cbr-xml-daily.ru/daily_json.js')
+    result = json.loads(response.text)
+    registr = currency.isupper()
+    currency_1 = 0
+    if registr == False:
+        currency_1 = currency.upper()
+    else:
+        currency_1 = currency
+    try:
+        res = f"Курс {currency_1} на {result['Date'][0:10]} по отношению к рублю " \
+              f"составляет {result['Valute'][currency_1]['Value']}"
+        print(res)
+    except KeyError:
+        print("None")

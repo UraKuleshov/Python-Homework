8d2c86010bc515d20dcd952b1dc5aea120e1b37e
Юрий Кулешов <kulesovurij817@gmail.com>
2022-07-04 18:41:47 +0300

    Second commit

--- Lesson_10.py
+++ Lesson_10.py
@@ -21,25 +21,25 @@ a.__add__()
 
 
 # task 2
-# class Clothes:
-#     def __init__(self, clothes):
-#         self.clothes = clothes
-#         self.a = {}
-#
-#     def expenditure_coat(self):
-#         result = self.clothes["пальто"] / 6.50 + 0.5
-#         self.a["пальто"] = result
-#
-#     def expenditure_suit(self):
-#         result = self.clothes["костюм"] * 2 + 0.3
-#         self.a["костюм"] = result
-#
-#     def amount(self):
-#         v = self.a["костюм"] + self.a["пальто"]
-#         print(f'Итоговый расход ткани составляет: {v}')
-#
-#
-# e = Clothes({"пальто": 20, "костюм": 30})
-# e.expenditure_coat()
-# e.expenditure_suit()
-# e.amount()
+class Clothes:
+    def __init__(self, clothes):
+        self.clothes = clothes
+        self.a = {}
+
+    def expenditure_coat(self):
+        result = self.clothes["пальто"] / 6.50 + 0.5
+        self.a["пальто"] = result
+
+    def expenditure_suit(self):
+        result = self.clothes["костюм"] * 2 + 0.3
+        self.a["костюм"] = result
+
+    def amount(self):
+        v = self.a["костюм"] + self.a["пальто"]
+        print(f'Итоговый расход ткани составляет: {v}')
+
+
+e = Clothes({"пальто": 20, "костюм": 30})
+e.expenditure_coat()
+e.expenditure_suit()
+e.amount()
